77860335d687e703c0517e0f1e565425285f980f
estherkimyunjung <estherkimyunjung@gmail.com>
2020-05-06 13:45:56 -0500

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -3,15 +3,16 @@ require "pry"
 
 def collect_multiples(limit)
   
-    num = 1
-    result = []
-    while num < limit
-        if (num % 3 == 0) || (num % 5 == 0)
-            result << num
-        end
-        num += 1
-    end
-    result
+    # num = 1
+    # result = []
+    # while num < limit
+    #     if (num % 3 == 0) || (num % 5 == 0)
+    #         result << num
+    #     end
+    #     num += 1
+    # end
+    # result
+    (1...limit).to_a.select{|num| num % 3 == 0 || num % 5 == 0}
 end
 
 def sum_multiples(limit)
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -13,15 +13,16 @@ class Multiples
     end
 
     def collect_multiples
-        num = 1
-        result = []
-        while num < limit
-            if (num % 3 == 0) || (num % 5 == 0)
-                result << num
-            end
-            num += 1
-        end
-        result
+        # num = 1
+        # result = []
+        # while num < limit
+        #     if (num % 3 == 0) || (num % 5 == 0)
+        #         result << num
+        #     end
+        #     num += 1
+        # end
+        # result
+        (1...limit).to_a.select{|num| num % 3 == 0 || num % 5 == 0}
     end
 
     def sum_multiples
